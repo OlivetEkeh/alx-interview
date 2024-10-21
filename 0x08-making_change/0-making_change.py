b3de0b4b5fbba2310a6e88767354de5b4dcebfe6
OlivetEkeh <ekehsons@gmail.com>
2024-10-22 00:16:51 +0100

    added a new file

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3
-"""Change comes from within"""
+"""
+Determines the fewest num of coins needed to meet a given amount total
+"""
 
 
 def makeChange(coins, total):
-    """determine the fewest number of coins needed to
-    meet a given amount total.
+    """
+    Calculates the minimum num of coins needed to make change for a given total
+    Returns:
+    The minimum num of coins needed to make change, or -1 if it is not possible
     """
     if total <= 0:
         return 0
-    dp = [0] + [float("inf")] * (total)
-    for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-    return dp[-1] if dp[-1] != float("inf") else -1
+
+    remaining = total
+    coins_count = 0
+    coin_num = 0
+    sorted_coins = sorted(coins, reverse=True)
+    u = len(coins)
+
+    while remaining > 0:
+        if coin_num >= u:
+            return -1
+        if remaining - sorted_coins[coin_num] >= 0:
+            remaining -= sorted_coins[coin_num]
+            coins_count += 1
+        else:
+            coin_num += 1
+    return coins_count
